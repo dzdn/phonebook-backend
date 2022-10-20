a07072ec81e6e1b25d7e6b7f18e3e0943ec2cb75
dzdn <jdustinwilliams@gmail.com>
2022-10-20 15:58:12 -0400

    Complete 3.4 (DELETE a person).

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
