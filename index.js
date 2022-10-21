e2b4cfbfb874c958f01e03606878d6bf505af3ce
dzdn <jdustinwilliams@gmail.com>
2022-10-20 21:16:39 -0400

    Prepare for deployment (OR with process.env.PORT).

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -100,7 +103,7 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
