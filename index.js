c8b03c317d054a2374b4e00942eb543f162a164d
dzdn <jdustinwilliams@gmail.com>
2022-10-20 21:29:06 -0400

    Complete 3.11 (make fullstack by copying 'build' from frontend).

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 morgan.token('body', (request, response) => {
     console.log(response.body)
     return JSON.stringify(request.body)
