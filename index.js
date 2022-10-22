4161276aff3c2a587907f719eab5292c58fe5c63
dzdn <jdustinwilliams@gmail.com>
2022-10-22 18:54:51 -0400

    Complete 3.15 (make delete work with database).

--- index.js
+++ index.js
@@ -108,9 +108,12 @@ app.post('/api/persons', (request, response) => {
         })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
